2188310849c28459766f8b00eebf23b68d474aa3
avaniklsjoshi <avaniklsjoshi@gmail.com>
2017-07-11 17:01:21 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,23 +8,5 @@ import {User} from './shared/model/User'
 })
 
 export class AppComponent{
-  title = 'basic';
-  message = 'My First Angular App';
-  users: User[] = [
-    {id: 25, name: 'Avani', username: 'avanijoshi'},
-    {id: 26, name: 'Baba', username: 'babajoshi'},
-    {id: 27, name: 'OOpi', username: 'oopig'}
-  ];
-
-  activeUser: User;
-
-  selectUser(user){
-    this.activeUser=user;
-    console.log(this.activeUser);
-  }
-
-  onUserCreated(event){
-    // console.log(event);
-    this.users.push(event.user);
-  }
+ 
 }
